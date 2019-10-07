850d9468b3b2cf281ef890717e035626aea2223d
Christian Czichy <christian@czichy.com>
2019-10-08 00:35:36 +0200

    get report

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ extern crate tokio;
 //use std::io::{self, Cursor};
 //use futures::Future;
 use regex::Regex;
-use reqwest::Client; //, Response};
+use reqwest::{Client};//, Response};
 use serde::Deserialize;
 //use serde_xml_rs::{from_str, to_string};
 use env_logger::Env;
@@ -15,24 +15,23 @@ use log::{error, info};
 use reqwest::StatusCode;
 use std::error::Error;
 use std::fs::File;
-use std::io::copy;
+use std::io::Write;
 use std::time::Duration;
-use tempfile::Builder;
+//use tempfile::Builder;
 
 #[derive(Deserialize, Debug)]
-struct Slideshow {
-    title: String,
-    author: String,
-}
-
-#[derive(Deserialize, Debug)]
-struct SlideshowContainer {
-    slideshow: Slideshow,
+struct FlexStatementResponse {
+    #[serde(rename = "Status")]
+    status: String,
+    #[serde(rename = "ErrorCode")]
+    errorcode: u16,
+    #[serde(rename = "ErrorMessage")]
+    errormessage: String,
 }
 
 fn get(client: &reqwest::Client, uri: &str) -> Result<reqwest::Response, Box<dyn Error>> {
     let mut retries = 3;
-    let mut delay = 1;
+    let mut delay = 3;
     loop {
         match client.get(uri).send() {
             Ok(response) => {
@@ -97,14 +96,14 @@ fn request_ref_code(client: &reqwest::Client) -> String {
     }
     panic!("Unexpected response");
 }
-fn request_report(client: &reqwest::Client, reference_code: String) -> String {
+fn request_report(client: &reqwest::Client, reference_code: String) -> Result<reqwest::Response, Box<dyn Error>> {
     let mut retries = 3;
     let mut delay = 1;
-    //et mut fail_count = 0;
     let statement_url = format!("https://gdcdyn.interactivebrokers.com/Universal/servlet/FlexStatementService.GetStatement?q={}&t={}&v={}",
                                  reference_code, "168331604313807293250457", 3);
 
     while retries > 0 {
+        info!("retries: {} fetching report: {}", retries, &statement_url);
         match get(client, &statement_url) {
             Err(error) => {
                 error!("{}", error);
@@ -114,16 +113,17 @@ fn request_report(client: &reqwest::Client, reference_code: String) -> String {
                 StatusCode::OK => {
                     match response.text() {
                         Ok(text) => {
-                            let _re = Regex::new(r">(?P<error>.*)</ErrorCode>").unwrap();
-                            match _re.captures(&text) {
-                                Some(m) => {
-                                    match m.name("error").unwrap().as_str().parse::<i32>().unwrap()
-                                    {
-                                        1018..=1019 => info!("1018 -- {:#?}", text),
+                            let statement_response:Result<FlexStatementResponse,serde_xml_rs::Error> = serde_xml_rs::from_str(&text);
+                            match statement_response {
+                                Ok(resp) => {
+                                match resp.errorcode{
+                                        1019 => info!("1019 -- {:#?}", text),
                                         _ => panic!("errororor"),
-                                    }
                                 }
-                                _ => return text,
+                                }
+                                _=> {
+                                    return Ok(response);
+                                }
                             }
                         }
                         Err(error) => {
@@ -148,77 +148,6 @@ fn request_report(client: &reqwest::Client, reference_code: String) -> String {
     }
     panic!("Unexpected response");
 }
-//client
-//        .get(&_url)
-//        .send()
-//        .and_then(|mut res| {
-//            println!("{}", res.status());
-//            res.text()
-//        })
-//        .map_err(|err| println!("request error: {}", err))
-//        .map(|body| {
-//            let re = Regex::new(r">(?P<ref>.*)</ReferenceCode>").unwrap();
-//            match re.captures(&body) {
-//                Some(m) => Ok(m.name("ref").map_or("".into(), |m| m.as_str().to_string())),
-//                _ => Err("kaputt"),
-//            }
-//        });
-//    request_code
-//    .map(|ref_code| {
-// let statement_url = format!("https://gdcdyn.interactivebrokers.com/Universal/servlet/FlexStatementService.GetStatement?q={}&t={}&v={}",
-//                             ref_code.unwrap(), "168331604313807293250457", 3);
-//        println!("{:#?}",statement_url);
-//        statement_url
-//    });
-//}
-//fn fetch() -> impl Future<Item = (), Error = ()> {
-//    let client = Client::new();
-//    let _url = format!(
-//        "https://gdcdyn.interactivebrokers.com/Universal/servlet/FlexStatementService.SendRequest?t={}&q={}&v={}",
-//        "168331604313807293250457", "381004", 3
-//    );
-//    let request_code =
-//client
-//        .get(&_url)
-//        .send()
-//        .and_then(|mut res| {
-//            println!("{}", res.status());
-//            res.text()
-//        })
-//        .map_err(|err| println!("request error: {}", err))
-//        .map(|body| {
-//            let re = Regex::new(r">(?P<ref>.*)</ReferenceCode>").unwrap();
-//            match re.captures(&body) {
-//                Some(m) => Ok(m.name("ref").map_or("".into(), |m| m.as_str().to_string())),
-//                _ => Err("kaputt"),
-//            }
-//        })
-//        .map(|ref_code| {
-//     let statement_url = format!("https://gdcdyn.interactivebrokers.com/Universal/servlet/FlexStatementService.GetStatement?q={}&t={}&v={}",
-//                                 ref_code.unwrap(), "168331604313807293250457", 3);
-//            println!("{:#?}",statement_url);
-//            statement_url
-//        });
-//
-//    request_code.and_then(|url| {
-//            let mut retries = 3;
-//    let mut delay = 1;
-//    loop {
-//        Client::new()
-//            .get(&url)
-//            .send()
-//            .and_then(|mut res| res.text())
-//            .map_err(|err| println!("request error: {}", err))
-//            .map(|body| {
-//                println!("{:#?}", body);
-//            });
-//            std::thread::sleep(Duration::from_secs(delay));
-//        retries -= 1;
-//        delay *= 16;
-//    }
-//
-//    })
-//}
 
 //fn main() {
 //    tokio::run(fetch());
@@ -238,26 +167,11 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     env_logger::from_env(Env::default().default_filter_or("info")).init();
     let client = Client::new();
     let reference_code = request_ref_code(&client);
-    let tmp_dir = Builder::new().prefix("example").tempdir()?;
-    let statement_url = format!("https://gdcdyn.interactivebrokers.com/Universal/servlet/FlexStatementService.GetStatement?q={}&t={}&v={}",
-                                 reference_code, "168331604313807293250457", 3);
-    let mut response = get(&client, &statement_url)?;
-    let rep = request_report(&client, reference_code);
-    info!("{:#?}", rep);
-    let mut dest = {
-        let fname = response
-            .url()
-            .path_segments()
-            .and_then(|segments| segments.last())
-            .and_then(|name| if name.is_empty() { None } else { Some(name) })
-            .unwrap_or("tmp.bin");
-
-        println!("file to download: '{}'", fname);
-        let fname = tmp_dir.path().join(fname);
-        println!("will be located under: '{:?}'", fname);
-        File::create(fname)?
-    };
-    info!("{:#?}", response);
-    copy(&mut response, &mut dest)?;
+    //let tmp_dir = Builder::new().prefix("example").tempdir()?;
+    let mut response = request_report(&client, reference_code)?;
+    let mut _dest = File::create("/home/czichy/tmp/test.xml")?;
+    //info!("Response: {:#?}", response);
+ _dest.write_all(&response.text()?.as_bytes())?;
+   // response.copy_to(&mut _dest)?;
     Ok(())
 }
